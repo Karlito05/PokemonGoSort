5b077cdc4627e381f8e76f9ed7cec4be28be4c40
unknown <karelos101@gmail.com>
2025-03-18 18:55:02 +0100

    made a basic version that pulls name and attacks from game
    Issues: based on lag the possitioning system is off

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
 import PIL
-import pytesseract
+import easyocr
 import pyautogui
 import bs4
+from time import sleep
 
+data = []
+
+for _ in range(0,10):
+    reader = easyocr.Reader(['en'])
+
+    screenshot = PIL.ImageGrab.grab(bbox=[1650, 590, 2100, 635])
+    screenshot.save("image.png")
+
+    name = reader.readtext("image.png")[0][1]
+
+    pyautogui.moveTo(1650, 690)
+    pyautogui.dragTo(1650, 545, duration=1)
+    pyautogui.click()
+
+    sleep(1)
+
+    screenshot = PIL.ImageGrab.grab(bbox=[1690, 1225, 1876, 1270])
+    screenshot.save("image.png")
+
+    attack1 = reader.readtext("image.png")[0][1]
+
+    screenshot = PIL.ImageGrab.grab(bbox=[1690, 1260, 1876, 1320])
+    screenshot.save("image.png")
+
+    attack2 = reader.readtext("image.png")[0][1]
+
+    data.append((name, (attack1, attack2)))
+    
+    pyautogui.moveTo(2000, 740)
+    pyautogui.dragRel(-500,0, duration=1)
+
+    sleep(2)
+
+for thing in data:
+    print(thing[0], thing[1][0], thing[1][1])
